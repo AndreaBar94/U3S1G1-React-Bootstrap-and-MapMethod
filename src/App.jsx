612a8e449f036428e322e799c9a71d8d2f6cacdc
AndreaBar94 <122469003+AndreaBar94@users.noreply.github.com>
2023-03-29 11:04:37 +0200

    bootstrap and react-bootstrap added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-import Header from './components/Header';
+import "bootstrap/dist/css/bootstrap.min.css";
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./components/Header";
 
 function App() {
-  return (
-    <div className="App">
-      <Header logoPath={logo}/>
-    </div>
-  );
+	return (
+		<div className="App">
+			<Header logoPath={logo} />
+		</div>
+	);
 }
 
 export default App;
