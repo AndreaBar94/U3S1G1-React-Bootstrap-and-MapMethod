ea77643dcb8c1bda02a258d238e40e245832e523
AndreaBar94 <122469003+AndreaBar94@users.noreply.github.com>
2023-03-29 15:12:25 +0200

    edit fontsize

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -2,8 +2,8 @@ function Welcome(){
     return(
         <div class="jumbotron jumbotron-fluid p-5 bg-warning bg-opacity-25">
             <div class="container bg-warning rounded p-3 bg-opacity-50 text-center">
-                <h1 class="display-4">Welcome</h1>
-                <p class="lead">Food for your mind!</p>
+                <h1 class="display-5">Welcome in the EpiBooks Shop!</h1>
+                <p class="lead">Feed for your mind</p>
             </div>
         </div>
     )
